e174d8d770c20e54d48a5724215c5df45658a654
Rachel Murray <dcbeergoddess@gmail.com>
2020-09-26 14:03:01 -0400

    making delete span work and remove elements from page inluding it's parent element the li

--- JQUERY_15_TODOAPP/assets/js/todos.js
+++ JQUERY_15_TODOAPP/assets/js/todos.js
@@ -1,9 +1,24 @@
-//Check Off Specific Todos by Clicking | Turn Gray w/ Strickthrough
+//Check Off Specific Todos by Clicking | Turn Gray w/ Strikethrough | REFACTORED
 $("li").click(function(){
   $(this).toggleClass("completed");
 })
 
-/*==================== REFACTORED USING CLASSES ABOVE ==============================
+//Click on X to delete todo
+//Don't trigger events on parent elements
+    //pass arugment into callback function | most common to use event or e
+    //e.stopPropagation()
+    //need to effect enclosing element in remove/fadeOut/etc | use: .parent()
+$('span').click(function(e){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove(); //not same as $(this) in line 12 // we are referring to the li here
+  })
+  e.stopPropagation();
+})
+
+
+
+/*==================== REFACTORED USING CLASS COMPLETED LINES 1-4==============================
+//Check Off Specific Todos by Clicking | Turn Gray w/ Strikethrough
 $('li').click(function(){
   //if li is gray
   //need to use rgb as you can see when you console.log($(this).css("color")) it prints rbg for gray when comparing in if statement
