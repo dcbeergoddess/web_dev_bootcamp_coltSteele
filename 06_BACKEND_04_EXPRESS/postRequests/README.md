edb151b60cbdd135bc29dfbdc256e1aff3716ae2
Rachel Murray <dcbeergoddess@gmail.com>
2020-10-04 16:13:02 -0400

    second part of POST requests, using body-parser to send data from form as js object

--- 06_BACKEND_04_EXPRESS/postRequests/README.md
+++ 06_BACKEND_04_EXPRESS/postRequests/README.md
@@ -1,10 +1,28 @@
 # POST Requests!!!!!!
 
 * Write POST routes, and test them with Postman
-  - 
+  - `app.post()`
+  - triggered by post requests
+  - set it up in postman to check it
 
 * Use a form to send a post request
-  - 
+  - when send form
+  - provide ACTION and METHOD
+    - action-where "/addfriend"
+    - method-post
+    - using "name" to provide "key"
+
+  - `res.redirect` to a `app.get()` route after form data processed in POST route
 
 * Use body parser to get form data
-  - 
+  - `npm install body-parser --save`
+  - and require in
+  - tell app to use it `app.use(bodyParser.urlencoded({extended: true}));`
+```js
+  //req.body is an object that will contain all the data from the request body
+  //all form data is in the req.body
+  //i.e. newFriend=bob = undefined if don't have body parser
+  //express does not create a req.body out of the box
+  //need to tell it to take body and turn it into an object for us
+```
+  - anytime we have a form that a user enters data into that we want to extract the data from on the server side we need to use **BODY-PARSER**
--- 06_BACKEND_04_EXPRESS/postRequests/app.js
+++ 06_BACKEND_04_EXPRESS/postRequests/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
+
+//FIRST REQUIRE BODY-PARSER
+const bodyParser = require('body-parser');
+
 const PORT = 8080;
 
 app.use(express.static("public"))
+
+//TELL EXPRESS TO USE BODY-PARSER
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs")
+
+let friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -11,13 +21,20 @@ app.get("/", (req, res) => {
 
 //show a list of friends
 app.get("/friends", (req, res) => {
-  let friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
   res.render("friends", {friends: friends});
 })
 
 //post a new friend
 app.post("/addFriend", (req, res) => {
-  res.send("YOU HAVE REACHED THE POST ROUTE")
+  //req.body is an object that will contain all the data from the request body
+  //all form data is in the req.body
+  //i.e. newFriend=bob = undefined if don't have body parser
+  //express does not create a req.body out of the box
+  //need to tell it to take body and turn it into an object for us
+  const newFriend = req.body.newfriend;
+  friends.push(newFriend);
+
+  res.redirect("/friends");
 })
 
 app.listen(PORT, () => {
