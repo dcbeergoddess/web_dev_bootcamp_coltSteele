60ace2ecaa344f81d3c9e2a41da41a2af536b996
Rachel Murray <dcbeergoddess@gmail.com>
2020-10-05 16:57:58 -0400

    updating weather app with new API key

--- 06_BACKEND_05_APIS/weatherApp/app.js
+++ 06_BACKEND_05_APIS/weatherApp/app.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 const dotenv = require('dotenv').config()
+const API_KEY = process.env.API_KEY
 
-axios.get("https://api.openweathermap.org/data/2.5/weather?q=honolulu&appid=" + process.env.API_KEY)
+axios.get("https://api.openweathermap.org/data/2.5/weather?q=honolulu&appid=" + API_KEY)
   .then((res) => {
     // console.log(res.data)
     console.log(res.data.sys.sunset)
